95630487eef7bb388eece30f2d7568b01403722f
AlexNush <nushtaikin@yandex.ru>
2024-07-25 11:29:50 +0300

    Create MenuApp

---
+++ testassignments/menuapp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=200)
+    menu = models.ForeignKey(
+        Menu, blank=True, related_name='items', on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey('self', null=True,
+                               blank=True, related_name='children',
+                               on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
